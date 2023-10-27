965b5af6f386681ab2c3989564c0225d618120a1
Akshat Gupta <guptaakshat370@gmai.com>
2023-10-27 18:11:21 +0530

    First Commit

---
+++ Introduction.js
@@ -0,0 +1,42 @@
+/*
+    Q. Why react was created ?
+        Ghost Message Problem 
+    
+    React is a library
+
+    Topics : 
+        
+        1. Core of React
+        2. Component Reusability
+        3. Reusing of Compnent (Props)
+        5. How to propogate change (hooks)
+
+    Additional Addon to React :
+
+        1. Router (React doesn't have Router)
+        2. State Management (React doesn't have State Management)
+            -> Redux, Redux Toolkit, zustand, context API
+        3. Class based component
+        4. BAAS Apps (Backend As a Service)
+            -> using it can make Social Media Clone , E-Commerce App
+        
+        React is not a complete solution 
+        -> No SEO, browser render of JS, no routing 
+
+        After React : 
+        Framework : NextJS, Gatsby , Remix
+
+        Dcoumentation : react.dev   
+
+                -----> React-DOM  (For Web)
+        React  |
+                -----> React-native (For Mobile Dev)
+        
+        These two attachments come with the react library.
+
+        npx create-react-app   -> bulky and takes time
+
+        better way :  using bundler (vite or Parcel) 
+        npm create vite@latest
+        need to include node_modules -> npm i ;
+*/
